e8a7760de9162ab569573b3c53e0ee152f73ab31
daanjiri <da.jimenez166@gmail.com>
2024-02-22 21:15:41 -0800

    set button for texture

--- src/pages/Model.tsx
+++ src/pages/Model.tsx
@@ -1,10 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { MeshBasicMaterial, MeshStandardMaterial } from 'three';
 
-export default function Model(props: any) {
+export default function Model({ useGoldenMaterial, ...props }: any) {
   const group = useRef();
-  const { scene } = useGLTF('/poporo1.glb') as any;
-  return <primitive object={scene} {...props} />;
+  const { scene } = useGLTF('/poporo.glb') as any;
+  useEffect(() => {
+    scene.traverse((child: any) => {
+      if (child.isMesh) {
+        // This replaces the material with a basic material, removing the texture
+        child.material = new MeshStandardMaterial({
+          color: useGoldenMaterial ? '#FFD700' : 'gray', // Golden or gray
+          roughness: useGoldenMaterial ? 0.4: 0.5,
+          metalness: useGoldenMaterial ? 1 : 0.1,
+        });
+      }
+    });
+  }, [scene, useGoldenMaterial]);
+  return <primitive object={scene} ref={group} {...props} />;
 }
 
-useGLTF.preload('/poporo1.glb');
+useGLTF.preload('/poporo.glb');
--- src/pages/ModelViewer.tsx
+++ src/pages/ModelViewer.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls, Stage } from '@react-three/drei';
 
@@ -12,16 +12,28 @@ const handleDownload = () => {
 };
 
 export default function ModeViewer() {
+  const [useGoldenMaterial, setUseGoldenMaterial] = useState(false);
+
+  const toggleMaterial = () => {
+    setUseGoldenMaterial(!useGoldenMaterial);
+  }
+
   return (
     <div>
       <Canvas style={{ height: '95vh' }}>
-        <ambientLight />
+        {/* <Environment preset="sunset" background={false} /> */}
+        <ambientLight /> 
         <pointLight position={[10, 10, 10]} />
         <pointLight position={[-10, -10, -10]} />
+        {/* <ambientLight intensity={20} /> 
+        {/* <directionalLight position={[0, 10, 5]} intensity={20} castShadow />  */}
+        {/* <pointLight position={[10, 20, 10]} intensity={8} /> */}
+        {/* <pointLight position={[-10, -10,-10]} intensity={8}/>  */}
         <OrbitControls />
-        <Model scale={0.03} />
+        <Model scale={0.03} useGoldenMaterial={useGoldenMaterial}/>
       </Canvas>
       <button onClick={handleDownload}>Download</button>
+      <button onClick={toggleMaterial}>Toggle Golden Material</button>
     </div>
   );
 }
